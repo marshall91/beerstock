f8d89f819fba8261823803be8e6c92cf2e6046b3
Mackenzie Marshall <mack.j.marshall@gmail.com>
2014-04-03 22:03:52 -0700

    Fixed untappd lib

--- Untappd.py
+++ Untappd.py
@@ -7,55 +7,61 @@ UNTAPPD_CLIENT_SECRET = "805F76302BD227868AF94F897EE041E82E4DE611"
 
 
 def UntappdSearch(query):
-	params = urllib.urlencode({'client_id': UNTAPPD_CLIENT_ID, 'client_secret': UNTAPPD_CLIENT_SECRET, 'q': query, 'sort': 'count'}) 
-	conn = httplib.HTTPConnection("api.untappd.com")
-	conn.request("GET", "/v4/search/beer?"+params)
-	response = conn.getresponse() 
-	jsonResponse = json.loads(response.read())
-	conn.close()
+    params = urllib.urlencode({'client_id': UNTAPPD_CLIENT_ID, 'client_secret': UNTAPPD_CLIENT_SECRET, 'q': query, 'sort': 'count'})
+    conn = httplib.HTTPConnection("api.untappd.com")
+    conn.request("GET", "/v4/search/beer?"+params)
+    response = conn.getresponse()
+    json_response = json.loads(response.read())
+    conn.close()
 
-	return jsonResponse
+    return json_response
+
 
 def UntappdCheckout(auth, bid):
-	params = urllib.urlencode({'gmt_offset': -8, 'timezone': 'PST', 'bid': bid})
-	url = "http://api.untappd.com/v4/checkin/add?access_token="+auth
-	response = urllib2.urlopen(url, params).read()
-	jsonResponse = json.loads(response)
+    params = urllib.urlencode({'gmt_offset': -8, 'timezone': 'PST', 'bid': bid})
+    url = "http://api.untappd.com/v4/checkin/add?access_token="+auth
+    response = urllib2.urlopen(url, params).read()
+    json_response = json.loads(response)
 
-	return jsonResponse
+    return json_response
+
 
 def UntappdGetAuthToken(code):
-	params = urllib.urlencode({'client_id': UNTAPPD_CLIENT_ID, 'client_secret': UNTAPPD_CLIENT_SECRET, 'reponse_type': 'code', 'redirect_url' : "http://www.beerstock.ca/beers/account_auth", 'code': code}) 
-	conn = httplib.HTTPSConnection("untappd.com")
-	conn.request("GET", "/oauth/authorize/?"+params)
-	response = conn.getresponse() 
-	jsonResponse = json.loads(response.read())
-	conn.close()
-	if jsonResponse['meta']['http_code'] == 200:
-		token = jsonResponse['response']['access_token']
-	else:
-		token = "null"
+    params = urllib.urlencode({'client_id': UNTAPPD_CLIENT_ID, 'client_secret': UNTAPPD_CLIENT_SECRET,
+                               'response_type': 'code', 'redirect_url': "http://www.beerstock.ca/account/account_auth",
+                               'code': code})
+    conn = httplib.HTTPSConnection("untappd.com")
+    conn.request("GET", "/oauth/authorize/?"+params)
+    response = conn.getresponse()
+    json_response = json.loads(response.read())
+    conn.close()
+    if json_response['meta']['http_code'] == 200:
+        token = json_response['response']['access_token']
+    else:
+        token = "null"
 
-	return token
+    return token
+
 
 def GetUntappdClientId():
-	return UNTAPPD_CLIENT_ID
+    return UNTAPPD_CLIENT_ID
+
 
 def JsonToBeer(entry):
-	newBeer = BeerTable()
-	newBeer.untappdId = entry['beer']['bid']
-	newBeer.name = entry['beer']['beer_name']
-	newBeer.style = entry['beer']['beer_style']
-	newBeer.imgUrl = entry['beer']['beer_label']
-	newBeer.abv = entry['beer']['beer_abv']
-	newBeer.breweryName = entry['brewery']['brewery_name']
-	newBeer.breweryId = entry['brewery']['brewery_id']
-	try:
-		dbBeer = BeerTable.objects.get(untappdId=newBeer.untappdId)	
-	except ObjectDoesNotExist:
-		newBeer.save()
-	dbBeer = BeerTable.objects.get(untappdId=newBeer.untappdId)
-	newBeer.id = dbBeer.id
+    new_beer = BeerTable()
+    new_beer.untappdId = entry['beer']['bid']
+    new_beer.name = entry['beer']['beer_name']
+    new_beer.style = entry['beer']['beer_style']
+    new_beer.imgUrl = entry['beer']['beer_label']
+    new_beer.abv = entry['beer']['beer_abv']
+    new_beer.breweryName = entry['brewery']['brewery_name']
+    new_beer.breweryId = entry['brewery']['brewery_id']
+    try:
+        db_beer = BeerTable.objects.get(untappdId=newBeer.untappdId)
+    except ObjectDoesNotExist:
+        new_beer.save()
+    db_beer = BeerTable.objects.get(untappdId=newBeer.untappdId)
+    new_beer.id = db_beer.id
 
-	return newBeer
+    return new_beer
 
