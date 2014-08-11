b2613d0a1e89cf68747ef6bc619cfae6ce4ecde1
Mackenzie Marshall <mack.j.marshall@gmail.com>
2014-08-10 21:28:44 -0700

    added modal for update beer

--- static/js/history.js
+++ static/js/history.js
@@ -11,5 +11,26 @@ window.onload = function () {
                 alert('AJAX failed');
             }
         });
-    })
+    });
+    var $updateButtons = $('._updateButton');
+    $.each($updateButtons, function () {
+        $(this).on('click', function () {
+            var id = $(this).val();
+            $.ajax({
+                url: '/beers/update_beer/' + id + '/',
+                type: 'get',
+                success: function (data) {
+                    var $modal = $('._updateModal' + id);
+                    if($modal.length === 0) {
+                        $('._beer_box' + id).append(data);
+                        $('._submit' + id).on('click', function () {
+                            $modal.find('form').submit();
+                        });
+                        $modal = $('._updateModal' + id);
+                    }
+                    $modal.modal();
+                }
+            });
+        });
+    });
 };
