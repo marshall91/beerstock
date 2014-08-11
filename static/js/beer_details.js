8167bd9f02cee12b2967339d80b86b81901f9821
Mackenzie Marshall <mack.j.marshall@gmail.com>
2014-08-10 22:40:44 -0700

    Minor fixes related to modals

--- static/js/beer_details.js
+++ static/js/beer_details.js
@@ -42,4 +42,19 @@ window.onload = function () {
             });
         })
     });
+
+    var $historyButton = $('._moreHistoryButton');
+    $historyButton.on('click', function () {
+        $.ajax({
+            url: '/beers/more_history/' + $historyButton.val() +'/',
+            type: 'get',
+            success: function (data) {
+                $('._moreHistory').before(data);
+                $historyButton.val(parseInt($historyButton.val())+1);
+            },
+            failure: function (data) {
+                alert('AJAX failed');
+            }
+        });
+    });
 };
